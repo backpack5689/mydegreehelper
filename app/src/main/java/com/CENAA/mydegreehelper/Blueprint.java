24f1b527636952bf67016d99be7f60ab4740003d
CeGyger92 <42616034+CeGyger92@users.noreply.github.com>
2021-03-07 22:30:40 -0600

    Initial work on BP Parser
    Open file function sends string to BP constructor where it begins to be parsed into BP data type.

--- app/src/main/java/com/CENAA/mydegreehelper/Blueprint.java
+++ app/src/main/java/com/CENAA/mydegreehelper/Blueprint.java
@@ -3,18 +3,50 @@ package com.CENAA.mydegreehelper;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Blueprint {
     int bpID;
+    String degree;
     int totalCredits;
-    String level;
+    int level;
     boolean bpComplete;
 
     ArrayList<Course> masterList;
     ArrayList<Requirement> requirements;
 
     public Blueprint(String bpInput){
-        //parse file to create BP
+    //parse file to create BP
+    Scanner sc = new Scanner(bpInput);
+    String line;
+    masterList = new ArrayList<Course>();
+    requirements = new ArrayList<Requirement>();
+    //Update level for Fresh/Soph/etc. (Possibly move this data elsewhere)
+    level = 1;
+    bpComplete = false;
+
+    //TO-DO replace 0 with auto-inc from DB
+    bpID = 0;
+    degree = sc.nextLine();
+    totalCredits = Integer.parseInt(sc.nextLine());
+    while(sc.hasNextLine()){
+        line = sc.nextLine();
+        Course course = new Course();
+        String[] splitLine = line.split("-");
+        course.courseName = splitLine[0];
+        course.courseSub = splitLine[1].split(" ")[0];
+        course.courseNum = Integer.parseInt(splitLine[1].split(" ")[1]);
+        for (int i = 2; i < splitLine.length; i++ ) {
+            if (splitLine[i].charAt(0) == '|') {
+
+            } else if (splitLine[i].charAt(0) == '(') {
+
+            } else if (splitLine[i].charAt(0) == '*') {
+
+            }
+        }
+    }
+
     }
 
     public boolean checkBPComplete(){
--- app/src/main/java/com/CENAA/mydegreehelper/Course.java
+++ app/src/main/java/com/CENAA/mydegreehelper/Course.java
@@ -4,25 +4,34 @@ import java.util.ArrayList;
 
 public class Course {
     String courseName;
+    String courseSub;
+    int courseNum;
     double grade;
     int creditValue;
     boolean completed;
-    ArrayList<Course> prerequsites;
+    ArrayList<Course> prereqs;
+    ArrayList<Course> equivalents;
 
     public Course(){
         courseName = "";
+        courseSub = "";
+        courseNum = 0000;
         creditValue = 0;
         grade = 0.0;
         completed = false;
-        prerequsites = new ArrayList<Course>();
+        prereqs = new ArrayList<Course>();
+        equivalents = new ArrayList<Course>();
     }
 
     public Course(String cName, int cr){
         courseName = cName;
+        courseSub = "";
+        courseNum = 0000;
         creditValue = cr;
         grade = 0.0;
         completed = false;
-        prerequsites = new ArrayList<Course>();
+        prereqs = new ArrayList<Course>();
+        equivalents = new ArrayList<Course>();
     }
 
 }
