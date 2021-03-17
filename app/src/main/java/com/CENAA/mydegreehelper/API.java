e98214473a51bdbe710283a7f072e2f82a4ffa8e
CEGyger92 <eddiegyger92@gmail.com>
2021-03-17 15:42:33 -0500

    Creation of POST Request API
    Staged for BP insertion to DB testing

---
+++ app/src/main/java/com/CENAA/mydegreehelper/API.java
@@ -0,0 +1,11 @@
+package com.CENAA.mydegreehelper;
+
+public class API {
+    private static final String ROOT_URL = "http://3.138.206.104/v1/api.php?apicall=";
+
+    public static final String URL_CREATE_BP = ROOT_URL + "createdegree";
+    public static final String URL_READ_BP = ROOT_URL + "getdegree";
+    public static final String URL_UPDATE_BP = ROOT_URL + "updatedegree";
+    public static final String URL_DELETE_BP = ROOT_URL + "deletedegree&id=";
+
+}
