137e90beb45aad58676e419ed6902aaaca3dd4bc
Conner Guidry <guidryconner@gmail.com>
2021-04-24 21:51:52 -0500

    Limit to 2 decimal places

--- app/src/main/java/com/CENAA/mydegreehelper/GradeInputFilter.java
+++ app/src/main/java/com/CENAA/mydegreehelper/GradeInputFilter.java
@@ -3,28 +3,26 @@ package com.CENAA.mydegreehelper;
 import android.text.InputFilter;
 import android.text.Spanned;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 class GradeInputFilter implements InputFilter {
-
-    private final double min;
-    private final double max;
-
-    public GradeInputFilter() {
-        this.min = 0.0;
-        this.max = 100.0;
-    }
 
     @Override
     public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
+        Pattern pattern = Pattern.compile("[0-9]{0,3}+((\\.[0-9]?)?)|(\\.)?"); // Regex check for 2 decimals
+        Matcher matcher = pattern.matcher(dest); // Match input to regex
+
+        // Check if input is in range (0-100)
         try {
             double input = Double.parseDouble(dest.toString() + source.toString());
-            if (isInRange(min, max, input)) {
+            if (isInRange(input) && matcher.matches()) {
                 return null;
             }
         } catch (NumberFormatException ignored) {}
         return "";
     }
 
-    private boolean isInRange(double a, double b, double c) {
-        return b > a ? c >= a && c <= b : c >= b && c <= a;
-    }
+    private boolean isInRange(double input) {
+        return 100.0 > 0.0 ? input >= 0.0 && input <= 100.0 : input >= 100.0 && input <= 0.0; }
 }
